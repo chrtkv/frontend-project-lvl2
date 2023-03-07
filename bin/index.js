9219c598474bda61b24d6f005a47f9c62381f2fb
Kirill Chertkov <chertkov.k@gmail.com>
2023-03-07 22:44:54 +0300

    1st step. refactor

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
 
-const package = require('../package.json');
+import { program } from 'commander';
 
-const { Command } = require('commander');
-const program = new Command();
 program
-  .version(package.version)
-  .description(package.description)
-  .arguments('<filepath1> <filepath2>')
-  .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]',  'output format');
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
 
-program.parse(process.argv);
+program.parse();
