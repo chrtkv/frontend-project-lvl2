7ab7f1acc8a54b1b6249bfe9cd0e928c83daf896
Kirill Chertkov <chertkov.k@gmail.com>
2023-03-08 02:38:40 +0300

    2nd step. add arguments and options

--- bin/index.js
+++ bin/index.js
@@ -5,5 +5,7 @@ import { program } from "commander";
 program
   .version("0.1.0")
   .description("Compares two configuration files and shows a difference.")
+  .arguments("<filepath1> <filepath2>")
+  .option("-f, --format <type>", "output format")
 
 program.parse();
