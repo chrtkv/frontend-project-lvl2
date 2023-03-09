29abc5a292391aca402cfdbe1a40a51713c195c7
Kirill Chertkov <chertkov.k@gmail.com>
2023-03-09 03:08:15 +0300

    add action to script

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
+import genDiff from "../src/genDiff.js"
 
 program
   .version("0.1.0")
   .description("Compares two configuration files and shows a difference.")
   .arguments("<filepath1> <filepath2>")
   .option("-f, --format <type>", "output format")
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
