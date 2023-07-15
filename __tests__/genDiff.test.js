35bccaa368401629452901008c3c6285e3071d3c
Kirill Chertkov <chertkov.k@gmail.com>
2023-07-16 01:35:44 +0300

    add test for different formats

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,6 +6,7 @@ import genDiff from '../src/genDiff';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// TODO: compose into test function with files as parameters
 test('genDiff with json', () => {
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const expectedResult = readFileSync(getFixturePath('result.txt'), 'utf-8');
@@ -19,6 +20,15 @@ test('genDiff with yaml', () => {
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const expectedResult = readFileSync(getFixturePath('result.txt'), 'utf-8');
   const fileName1 = 'before.yml';
+  const fileName2 = 'after.yml';
+  const result = genDiff(getFixturePath(fileName1), getFixturePath(fileName2));
+  expect(result).toEqual(expectedResult);
+});
+
+test('genDiff with different formats', () => {
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const expectedResult = readFileSync(getFixturePath('result.txt'), 'utf-8');
+  const fileName1 = 'before.json';
   const fileName2 = 'after.yml';
   const result = genDiff(getFixturePath(fileName1), getFixturePath(fileName2));
   expect(result).toEqual(expectedResult);
