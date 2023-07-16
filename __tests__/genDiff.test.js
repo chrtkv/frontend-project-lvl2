0a46d9ea28b21d85e85e0aaceae07ff8356078c9
Kirill Chertkov <chertkov.k@gmail.com>
2023-07-16 23:54:33 +0300

    update fixtures to recursive structures

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,10 +5,10 @@ import genDiff from '../src/genDiff';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 // TODO: compose into test function with files as parameters
 test('genDiff with json', () => {
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const expectedResult = readFileSync(getFixturePath('result.txt'), 'utf-8');
   const fileName1 = 'before.json';
   const fileName2 = 'after.json';
@@ -17,7 +17,6 @@ test('genDiff with json', () => {
 });
 
 test('genDiff with yaml', () => {
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const expectedResult = readFileSync(getFixturePath('result.txt'), 'utf-8');
   const fileName1 = 'before.yml';
   const fileName2 = 'after.yml';
@@ -26,7 +25,6 @@ test('genDiff with yaml', () => {
 });
 
 test('genDiff with different formats', () => {
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const expectedResult = readFileSync(getFixturePath('result.txt'), 'utf-8');
   const fileName1 = 'before.json';
   const fileName2 = 'after.yml';
