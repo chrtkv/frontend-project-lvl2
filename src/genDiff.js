84d51d2375c8ecbb21171640ec2076ee231f3762
Kirill Chertkov <chertkov.k@gmail.com>
2023-07-22 19:33:29 +0300

    add nested configs comparing

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,92 +1,84 @@
 import { readFileSync } from 'fs';
-import { extname, parse } from 'path';
-import parser from './parser';
+import { extname } from 'path';
+import parser from './parser.js';
 
-// TODO: split to modules
 const compareDataChanges = (oldData, newData) => {
-  const keys = [...new Set([...Object.keys(oldData), ...Object.keys(newData)])];
+  const isObject = (value) => typeof value === 'object' && !Array.isArray(value) && value !== null;
+  if (isObject(oldData) && isObject(newData)) {
+    const keys = [...new Set([...Object.keys(oldData), ...Object.keys(newData)])];
+    return keys.reduce((acc, key) => {
+      const result = compareDataChanges(oldData[key], newData[key]);
+      return { ...acc, [key]: result };
+    }, {});
+  }
 
-  return keys.reduce((acc, key) => { // eslint-disable-line array-callback-return, consistent-return
-    if (oldData[key] === newData[key]) {
-      return {
-        ...acc,
-        [key]: {
-          oldValue: oldData[key],
-          newValue: newData[key],
-          status: 'unchanged',
-        },
-      };
-    }
-    if (
-      Object.keys(oldData).includes(key)
-      && !Object.keys(newData).includes(key)
-    ) {
-      return {
-        ...acc,
-        [key]: {
-          oldValue: oldData[key],
-          newValue: null,
-          status: 'deleted',
-        },
-      };
-    }
-    if (
-      !Object.keys(oldData).includes(key)
-      && Object.keys(newData).includes(key)
-    ) {
-      return {
-        ...acc,
-        [key]: {
-          oldValue: null,
-          newValue: newData[key],
-          status: 'added',
-        },
-      };
-    }
-    if (oldData[key] !== newData[key]) {
-      return {
-        ...acc,
-        [key]: {
-          oldValue: oldData[key],
-          newValue: newData[key],
-          status: 'changed',
-        },
-      };
-    }
-  }, {});
+  if (oldData === newData) {
+    return { oldValue: oldData, newValue: newData, status: 'unchanged' };
+  }
+
+  if (oldData !== undefined && newData === undefined) {
+    return { oldValue: oldData, newValue: newData, status: 'deleted' };
+  }
+
+  if (oldData === undefined && newData !== undefined) {
+    return { oldValue: oldData, newValue: newData, status: 'added' };
+  }
+
+  return { oldValue: oldData, newValue: newData, status: 'changed' };
 };
 
-const formatOutput = (data) => {
-  const sortedKeys = Object.keys(data).sort();
-  const formattedLines = sortedKeys.reduce((acc, key) => {
-    const { status, oldValue, newValue } = data[key];
-    switch (status) {
-      case 'unchanged':
-        return [
-          ...acc,
-          `    ${key}: ${oldValue}`,
-        ];
-      case 'changed':
-        return [
-          ...acc,
-          `  - ${key}: ${oldValue}`,
-          `  + ${key}: ${newValue}`,
-        ];
-      case 'deleted':
-        return [
-          ...acc,
-          `  - ${key}: ${oldValue}`,
-        ];
-      case 'added':
-        return [
-          ...acc,
-          `  + ${key}: ${newValue}`,
-        ];
-      default:
-        throw new Error(`Invalid status: ${status}`);
+const formatIndent = (indent, symbol) => {
+  const cb = (char, index, arr) => ((arr.length - 2 === index) ? symbol : char);
+  return indent.split('').map(cb).join('');
+};
+
+const formatOutput = (diff, indentChar = ' ', indentCharsCount = 4) => {
+  const iter = (data, depth) => {
+    if (typeof data !== 'object') {
+      return data.toString();
     }
-  }, []);
-  return `{\n${formattedLines.join('\n')}\n}`;
+    if (data === null) {
+      return 'null';
+    }
+    const indent = indentChar.repeat(depth * indentCharsCount);
+    const bracketIndent = indentChar.repeat((depth * indentCharsCount) - indentCharsCount);
+    const sortedKeys = Object.keys(data).sort();
+    const formattedLines = sortedKeys.reduce((acc, key) => {
+      const { oldValue, newValue, status } = data[key];
+      switch (status) {
+        case 'unchanged':
+          return [
+            ...acc,
+            `${indent}${key}: ${oldValue}`,
+          ];
+        case 'changed':
+          return [
+            ...acc,
+            `${formatIndent(indent, '-')}${key}: ${iter(oldValue, depth + 1)}`,
+            `${formatIndent(indent, '+')}${key}: ${iter(newValue, depth + 1)}`,
+          ];
+        case 'deleted':
+          return [
+            ...acc,
+            `${formatIndent(indent, '-')}${key}: ${iter(oldValue, depth + 1)}`,
+          ];
+        case 'added':
+          return [
+            ...acc,
+            `${formatIndent(indent, '+')}${key}: ${iter(newValue, depth + 1)}`,
+          ];
+        default:
+          return [...acc, `${indent}${key}: ${iter(data[key], depth + 1)}`];
+      }
+    }, []);
+
+    return [
+      '{',
+      ...formattedLines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+  return iter(diff, 1);
 };
 
 const genDiff = (filePath1, filePath2) => {
@@ -95,6 +87,7 @@ const genDiff = (filePath1, filePath2) => {
   const file1data = parser(readFileSync(filePath1, 'utf-8'), file1extension);
   const file2data = parser(readFileSync(filePath2, 'utf-8'), file2extension);
   const comparedData = compareDataChanges(file1data, file2data);
+  // return JSON.stringify(comparedData);
   return formatOutput(comparedData);
 };
 
