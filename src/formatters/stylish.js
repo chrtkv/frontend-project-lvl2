de2d5f9d12467af35883e46042873cf5c1b20913
Kirill Chertkov <chertkov.k@gmail.com>
2023-08-04 02:29:24 +0300

    simplify code, replace reduce with map

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -27,26 +27,25 @@ export default (diff, indentChar = ' ', indentCharsCount = 4) => {
     const indent = indentChar.repeat(depth * indentCharsCount);
     const bracketIndent = indentChar.repeat((depth - 1) * indentCharsCount);
 
-    const formattedLines = data.reduce((acc, { value1, value2, key, type, children }) => {
+    const formattedLines = data.flatMap(({ value1, value2, key, type, children }) => {
       switch (type) {
         case 'nested':
-          return [...acc, `${indent}${key}: ${iter(children, depth + 1)}`];
+          return `${indent}${key}: ${iter(children, depth + 1)}`;
         case 'unchanged':
-          return [...acc, `${indent}${key}: ${formatValue(value1, depth + 1)}`];
+          return `${indent}${key}: ${formatValue(value1, depth + 1)}`;
         case 'updated':
           return [
-            ...acc,
             `${formatIndent(indent, '-')}${key}: ${formatValue(value1, depth + 1)}`,
             `${formatIndent(indent, '+')}${key}: ${formatValue(value2, depth + 1)}`,
           ];
         case 'removed':
-          return [...acc, `${formatIndent(indent, '-')}${key}: ${formatValue(value1, depth + 1)}`];
+          return `${formatIndent(indent, '-')}${key}: ${formatValue(value1, depth + 1)}`;
         case 'added':
-          return [...acc, `${formatIndent(indent, '+')}${key}: ${formatValue(value2, depth + 1)}`];
+          return `${formatIndent(indent, '+')}${key}: ${formatValue(value2, depth + 1)}`;
         default:
           throw new Error(`Unknown type: ${type}`);
       }
-    }, []);
+    });
 
     return ['{', ...formattedLines, `${bracketIndent}}`].join('\n');
   };
