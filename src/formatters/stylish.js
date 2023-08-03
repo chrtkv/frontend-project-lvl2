aff24ee8bd5c7ce2c37f9bb07ce7ea5efdbedaec
Kirill Chertkov <chertkov.k@gmail.com>
2023-08-03 21:19:01 +0300

    change stylish formatter to work with new diff tree

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,47 +6,15 @@ export default (diff, indentChar = ' ', indentCharsCount = 4) => {
     return indent.split('').map(cb).join('');
   };
 
-  const iter = (data, depth) => {
-    if (_.isNull(data)) {
-      return 'null';
+  const formatValue = (value, depth) => {
+    if (!_.isObject(value)) {
+      return `${value}`;
     }
-    if (!_.isPlainObject(data)) {
-      return data.toString();
-    }
+
     const indent = indentChar.repeat(depth * indentCharsCount);
-    const bracketIndent = indentChar.repeat((depth * indentCharsCount) - indentCharsCount);
-    const sortedKeys = _.sortBy(_.keys(data));
-    const formattedLines = sortedKeys.reduce((acc, key) => {
-      const { oldValue, newValue, status } = data[key];
-      if (!status) {
-        return [...acc, `${indent}${key}: ${iter(data[key], depth + 1)}`];
-      }
-      switch (status) {
-        case 'unchanged':
-          return [
-            ...acc,
-            `${indent}${key}: ${oldValue}`,
-          ];
-        case 'updated':
-          return [
-            ...acc,
-            `${formatIndent(indent, '-')}${key}: ${iter(oldValue, depth + 1)}`,
-            `${formatIndent(indent, '+')}${key}: ${iter(newValue, depth + 1)}`,
-          ];
-        case 'removed':
-          return [
-            ...acc,
-            `${formatIndent(indent, '-')}${key}: ${iter(oldValue, depth + 1)}`,
-          ];
-        case 'added':
-          return [
-            ...acc,
-            `${formatIndent(indent, '+')}${key}: ${iter(newValue, depth + 1)}`,
-          ];
-        default:
-          throw new Error(`Unknown status: ${status}`);
-      }
-    }, []);
+    const bracketIndent = indentChar.repeat((depth - 1) * indentCharsCount);
+    const formattedLines = Object.entries(value)
+      .map(([key, val]) => `${indent}${key}: ${formatValue(val, depth + 1)}`);
 
     return [
       '{',
@@ -54,5 +22,34 @@ export default (diff, indentChar = ' ', indentCharsCount = 4) => {
       `${bracketIndent}}`,
     ].join('\n');
   };
+
+  const iter = (data, depth) => {
+    const indent = indentChar.repeat(depth * indentCharsCount);
+    const bracketIndent = indentChar.repeat((depth - 1) * indentCharsCount);
+
+    const formattedLines = data.reduce((acc, { value1, value2, key, type, children }) => {
+      switch (type) {
+        case 'nested':
+          return [...acc, `${indent}${key}: ${iter(children, depth + 1)}`];
+        case 'unchanged':
+          return [...acc, `${indent}${key}: ${formatValue(value1, depth + 1)}`];
+        case 'updated':
+          return [
+            ...acc,
+            `${formatIndent(indent, '-')}${key}: ${formatValue(value1, depth + 1)}`,
+            `${formatIndent(indent, '+')}${key}: ${formatValue(value2, depth + 1)}`,
+          ];
+        case 'removed':
+          return [...acc, `${formatIndent(indent, '-')}${key}: ${formatValue(value1, depth + 1)}`];
+        case 'added':
+          return [...acc, `${formatIndent(indent, '+')}${key}: ${formatValue(value2, depth + 1)}`];
+        default:
+          throw new Error(`Unknown type: ${type}`);
+      }
+    }, []);
+
+    return ['{', ...formattedLines, `${bracketIndent}}`].join('\n');
+  };
+
   return iter(diff, 1);
 };
