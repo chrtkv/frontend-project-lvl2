92b1d0935028563a08579b6f1add056d2af89b54
Kirill Chertkov <chertkov.k@gmail.com>
2023-08-15 03:51:31 +0300

    use file formats instead of extensions to define suitable parser

--- src/parser.js
+++ src/parser.js
@@ -2,10 +2,10 @@ import YAML from 'yaml';
 
 export default (data, extension) => {
   switch (extension) {
-    case '.yml':
-    case '.yaml':
+    case 'yml':
+    case 'yaml':
       return YAML.parse(data);
-    case '.json':
+    case 'json':
       return JSON.parse(data);
     default:
       throw new Error(`Unknown extension ${extension}`);
