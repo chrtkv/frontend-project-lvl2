32a4c885ebe4d4fa78ba5222af290b0878bd55cc
Kirill Chertkov <chertkov.k@gmail.com>
2023-08-15 03:50:41 +0300

    change naming in main function

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,18 +1,16 @@
-import { readFileSync } from 'fs';
-import { extname } from 'path';
-import parse from './parser.js';
-import compare from './comparator.js';
 import getFormatter from './formatters/index.js';
+import parse from './parser.js';
+import { buildComparisonTree, extractFormat, readFile } from './utils.js';
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const extension1 = extname(filepath1);
-  const extension2 = extname(filepath2);
-  const data1 = parse(readFileSync(filepath1, 'utf-8'), extension1);
-  const data2 = parse(readFileSync(filepath2, 'utf-8'), extension2);
-  const comparedData = compare(data1, data2);
+  const fileFormat1 = extractFormat(filepath1);
+  const fileFormat2 = extractFormat(filepath2);
+  const parsedData1 = parse(readFile(filepath1), fileFormat1);
+  const parsedData2 = parse(readFile(filepath2), fileFormat2);
+  const comparisonTree = buildComparisonTree(parsedData1, parsedData2);
 
   const formatter = getFormatter(formatName);
-  const formattedDiff = formatter(comparedData);
+  const formattedDiff = formatter(comparisonTree);
   return formattedDiff;
 };
 
