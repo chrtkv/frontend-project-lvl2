f7215edda5d8d0748dd1c8e7c6d9eec19bac5e47
Kirill Chertkov <chertkov.k@gmail.com>
2023-08-18 02:45:37 +0300

    refactor tree builder

--- src/utils.js
+++ src/utils.js
@@ -12,37 +12,36 @@ const extractFormat = (filepath) => {
   return extension.slice(1);
 };
 
-const buildTree = (data1, data2, key = null) => {
-  if (_.isPlainObject(data1) && _.isPlainObject(data2)) {
-    const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
-    const children = keys
-      .map((keyName) => buildTree(data1[keyName], data2[keyName], keyName));
-
-    if (_.isNull(key)) {
-      return children;
+const buildTree = (data1, data2) => {
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+  return keys.map((key) => {
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return {
+        key,
+        type: 'nested',
+        children: buildTree(data1[key], data2[key]),
+      };
     }
 
-    return { key, type: 'nested', children };
-  }
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      return { key, type: 'removed', value1: data1[key] };
+    }
 
-  if (_.isUndefined(data1) && !_.isUndefined(data2)) {
-    return { key, type: 'added', value2: data2 };
-  }
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      return { key, type: 'added', value2: data2[key] };
+    }
 
-  if (!_.isUndefined(data1) && _.isUndefined(data2)) {
-    return { key, type: 'removed', value1: data1 };
-  }
+    if (!_.isEqual(data1[key], data2[key])) {
+      return {
+        key,
+        type: 'updated',
+        value1: data1[key],
+        value2: data2[key],
+      };
+    }
 
-  if (!_.isEqual(data1, data2)) {
-    return {
-      key,
-      type: 'updated',
-      value1: data1,
-      value2: data2,
-    };
-  }
-
-  return { key, type: 'unchanged', value1: data1 };
+    return { key, type: 'unchanged', value1: data1[key] };
+  });
 };
 
 export { buildTree, extractFormat, readFile };
