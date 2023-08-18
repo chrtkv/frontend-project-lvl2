f7e9181a29454a8e84df7ada3b67ad66f1bac0f0
Kirill Chertkov <chertkov.k@gmail.com>
2023-08-18 03:34:15 +0300

    change formatter interface

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,8 +1,18 @@
-import plainFormatter from './plain.js';
-import stylishFormatter from './stylish.js';
+import plainFormatter from './plain';
+import stylishFormatter from './stylish';
 
-export default {
-  json: JSON.stringify,
-  plain: plainFormatter,
-  stylish: stylishFormatter,
+export default (tree, formatName) => {
+  const formatterMapping = {
+    json: JSON.stringify,
+    plain: plainFormatter,
+    stylish: stylishFormatter,
+  };
+
+  const formatter = formatterMapping[formatName];
+
+  if (!formatter) {
+    throw new Error(`Unknown format: ${formatName}`);
+  }
+
+  return formatter(tree);
 };
