bb52d6f71b1237231608c774c3c2d227c02784d8
Kirill Chertkov <chertkov.k@gmail.com>
2023-08-19 02:43:19 +0300

    refactor stylish formatter, split operations into its own functions

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,60 +1,64 @@
 import _ from 'lodash';
 
-export default (diff, indentChar = ' ', indentCharsCount = 4) => {
-  const formatIndent = (indent, statusSign) => {
-    const cb = (char, index, arr) => ((arr.length - 2 === index) ? statusSign : char);
-    return indent.split('').map(cb).join('');
-  };
+const REMOVED_TYPE_SIGN = '-';
+const ADDED_TYPE_SIGN = '+';
+const OFFSET_FROM_END = 2; // Offset from the end of the indent
+const INDENT_CHAR = ' ';
+const INDENT_CHARS_COUNT = 4;
 
-  const iter = (data, depth) => {
-    const indent = indentChar.repeat(depth * indentCharsCount);
-    const bracketIndent = indentChar.repeat((depth - 1) * indentCharsCount);
+const formatIndent = (depth, typeSign = ' ') => {
+  const indent = INDENT_CHAR.repeat(depth * INDENT_CHARS_COUNT);
+  const cb = (char, index, arr) => ((arr.length - OFFSET_FROM_END === index) ? typeSign : char);
 
-    if (_.has(data, 'type')) {
-      const {
-        key,
-        type,
-        value1,
-        value2,
-        children,
-      } = data;
-
-      switch (type) {
-        case 'nested':
-          return `${indent}${key}: ${iter(children, depth + 1)}`;
-        case 'unchanged':
-          return `${indent}${key}: ${iter(value1, depth + 1)}`;
-        case 'updated':
-          return [
-            `${formatIndent(indent, '-')}${key}: ${iter(value1, depth + 1)}`,
-            `${formatIndent(indent, '+')}${key}: ${iter(value2, depth + 1)}`,
-          ];
-        case 'removed':
-          return `${formatIndent(indent, '-')}${key}: ${iter(value1, depth + 1)}`;
-        case 'added':
-          return `${formatIndent(indent, '+')}${key}: ${iter(value2, depth + 1)}`;
-        default:
-          throw new Error(`Unknown type: ${type}`);
-      }
-    }
-
-    if (_.isPlainObject(data)) {
-      const formattedLines = Object.entries(data)
-        .map(([key, value]) => `${indent}${key}: ${iter(value, depth + 1)}`);
-      return iter(formattedLines, depth);
-    }
-
-    if (!_.isObject(data)) {
-      return `${data}`;
-    }
-
-    const formattedLines = data.flatMap((item) => iter(item, depth));
-    return [
-      '{',
-      ...formattedLines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-
-  return iter(diff, 1);
+  return indent.split('').map(cb).join('');
 };
+
+const stringify = (data, depth) => {
+  if (!_.isObject(data)) {
+    return `${data}`;
+  }
+
+  const indent = formatIndent(depth);
+  const bracketIndent = formatIndent(depth - 1);
+
+  if (_.isPlainObject(data)) {
+    const formattedLines = Object.entries(data)
+      .map(([key, value]) => `${indent}${key}: ${stringify(value, depth + 1)}`);
+    return stringify(formattedLines, depth);
+  }
+
+  return [
+    '{',
+    ...data,
+    `${bracketIndent}}`,
+  ].join('\n');
+};
+
+const render = (diff, depth = 1) => stringify(diff
+  .flatMap(({
+    key,
+    type,
+    value1,
+    value2,
+    children,
+  }) => {
+    switch (type) {
+      case 'nested':
+        return `${formatIndent(depth)}${key}: ${render(children, depth + 1)}`;
+      case 'unchanged':
+        return `${formatIndent(depth)}${key}: ${stringify(value1, depth + 1)}`;
+      case 'updated':
+        return [
+          `${formatIndent(depth, REMOVED_TYPE_SIGN)}${key}: ${stringify(value1, depth + 1)}`,
+          `${formatIndent(depth, ADDED_TYPE_SIGN)}${key}: ${stringify(value2, depth + 1)}`,
+        ];
+      case 'removed':
+        return `${formatIndent(depth, REMOVED_TYPE_SIGN)}${key}: ${stringify(value1, depth + 1)}`;
+      case 'added':
+        return `${formatIndent(depth, ADDED_TYPE_SIGN)}${key}: ${stringify(value2, depth + 1)}`;
+      default:
+        throw new Error(`Unknown type: ${type}`);
+    }
+  }), depth);
+
+export default (diff) => render(diff);
