74bc460ef4b8908932db19bb1869e50f20b662d1
Kirill Chertkov <chertkov.k@gmail.com>
2023-08-18 03:35:25 +0300

    fix naming in parser function

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,13 @@
 import YAML from 'yaml';
 
-export default (data, extension) => {
-  switch (extension) {
+export default (data, fileFormat) => {
+  switch (fileFormat) {
     case 'yml':
     case 'yaml':
       return YAML.parse(data);
     case 'json':
       return JSON.parse(data);
     default:
-      throw new Error(`Unknown extension ${extension}`);
+      throw new Error(`Unknown file format: ${fileFormat}`);
   }
 };
