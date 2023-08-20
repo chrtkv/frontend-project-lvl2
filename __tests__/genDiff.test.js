a9d9c41be9cbd151c13d693ee498d2ee7b481840
Kirill Chertkov <chertkov.k@gmail.com>
2023-08-20 03:31:41 +0300

    refactor tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,35 +6,29 @@ import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const fixturesPath = `${__dirname}/../__fixtures__/`;
+const fixturesPath = `${__dirname}/__fixtures__/`;
 
-const getExpected = (formatterName) => readFileSync(`${fixturesPath}expected.${formatterName}.txt`, 'utf-8');
+const getExpected = (formatter) => readFileSync(`${fixturesPath}expected.${formatter}.txt`, 'utf-8');
 const getBeforeAfterFiles = (fileFormat) => [
-  fileFormat,
   `${fixturesPath}before.${fileFormat}`,
   `${fixturesPath}after.${fileFormat}`,
 ];
-const getCombinations = (formatters, formats) => formatters
-  .flatMap((formatter) => formats
-    .map(getBeforeAfterFiles)
-    .map((combintation) => [formatter, ...combintation]));
 
-const formattersList = ['plain', 'stylish', 'json'];
-const fileFormatsList = ['yml', 'json', 'yaml'];
-const combinations = getCombinations(formattersList, fileFormatsList);
+const formatters = ['plain', 'stylish', 'json'];
+const fileFormats = ['yml', 'json', 'yaml'];
 
-describe('genDiff tests', () => {
-  test.each(combinations)(
-    'use %s formatter with %s files',
-    (formatter, fileFormat, filepath1, filepath2) => {
-      const expectedResult = getExpected(formatter);
-      const actualResult = genDiff(filepath1, filepath2, formatter);
-      expect(actualResult).toEqual(expectedResult);
-    },
-  );
+describe.each(formatters)('%s formatter', (formatter) => {
+  it.each(fileFormats)('should work with %s file', (fileFormat) => {
+    const [filepath1, filepath2] = getBeforeAfterFiles(fileFormat);
+    const expectedResult = getExpected(formatter);
+    const actualResult = genDiff(filepath1, filepath2, formatter);
+    expect(actualResult).toEqual(expectedResult);
+  });
+});
 
-  test('use default formatter', () => {
-    const [, filepath1, filepath2] = getBeforeAfterFiles('json');
+describe('default formatter', () => {
+  it.each(fileFormats)('should be stylish and work with %s file', (fileFormat) => {
+    const [filepath1, filepath2] = getBeforeAfterFiles(fileFormat);
     const expectedResult = getExpected('stylish');
     const actualResult = genDiff(filepath1, filepath2);
     expect(actualResult).toEqual(expectedResult);
